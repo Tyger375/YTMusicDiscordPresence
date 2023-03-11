10c138a23bba18ac434ed93fe70f5346df435419
Tyger375 <edoardo.takanen@gmail.com>
2023-03-11 13:31:23 +0100

    First commit

---
+++ extension/content.js
@@ -0,0 +1,29 @@
+chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+            //return {cancel: details.url.indexOf("://music.youtube.com/") != -1};
+            let cookies = ""
+            let url = details.url;
+            if (url.includes("next"))
+                request("")
+            else if (url.includes("watchtime"))
+            {
+                if (url.includes("paused"))
+                    request("pause")
+                else if (url.includes("playing"))
+                    request("resume")
+            }
+        },
+    { urls: ["<all_urls>"] }
+);
+
+
+function request(action) {
+    fetch("http://127.0.0.1:5000/" + action,
+          {
+              method: "POST",
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+              }
+          })
+}
---
+++ server.py
@@ -0,0 +1,75 @@
+import nest_asyncio
+
+nest_asyncio.apply()
+
+import asyncio
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from ytmusicapi import YTMusic
+from pypresence import Presence
+
+client_id = "Insert your discord's app's client id"
+RPC = Presence(client_id=client_id)
+RPC.connect()
+
+ytmusic = YTMusic("headers_auth.json")
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+stopped = True
+
+
+async def nextSong():
+    await asyncio.sleep(1.5)
+    try:
+        updatePresence()
+    except Exception as e:
+        print(e)
+
+
+def updatePresence():
+    global stopped
+    stopped = False
+    song = ytmusic.get_history()[0]
+    img = song["thumbnails"][0]["url"]
+    print(song)
+
+    RPC.update(
+        buttons=[{"label": "Listen", "url": f"https://youtube.com/watch?v={song['videoId']}"}],
+        details=f"Listening to {song['title']}",
+        state=f"by {', '.join([artist['name'] for artist in song['artists']])}",
+        large_image=img
+    )
+
+
+@app.route("/", methods=["POST"])
+@cross_origin()
+def main():
+    asyncio.run(nextSong())
+    return "Presence updated"
+
+
+@app.route("/pause", methods=["POST"])
+@cross_origin()
+def pause():
+    global stopped
+    stopped = True
+    try:
+        RPC.clear()
+    except Exception as e:
+        print(e)
+    return "Presence updated"
+
+
+@app.route("/resume", methods=["POST"])
+@cross_origin()
+def resume():
+    if stopped:
+        updatePresence()
+    return "Presence updated"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
